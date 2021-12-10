7cebff5b14e952c40e8540f6287e532dbdd839ad
Marcio de Andrade <marcio@correnteeletrica.com.br>
2021-12-10 06:31:12 -0400

    Configuração
    Contexto do Banco de Dados, Registro do serviço

---
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        { }
+        public AppDbContext()
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.
+                       GetConnectionString("DefaultConnection"); //define usar a Connection String
+                //Configura que banco de dados será usado - No nosso caso SQL Server
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
